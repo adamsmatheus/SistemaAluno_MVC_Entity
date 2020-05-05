cfa52e64f6cd9618249a33265bcd650413060a0a
Matheus Adams <adamsmatheus7@gmail.com>
2020-05-05 20:12:40 -0300

    Add project files.

---
+++ SistemaAluno/Models/ControleContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+
+namespace SistemaAluno.Models
+{
+    public class ControleContext : DbContext
+    {
+        public ControleContext() : base("DefaultConnection")
+        {
+        
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+
+        public System.Data.Entity.DbSet<SistemaAluno.Models.Usuario> Usuarios { get; set; }
+    }
+}
